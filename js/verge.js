e945ac1be33ab0a4866c8e805e4f0e44cabf9534
Gayathri Mohan <gayathrimohan@qburst.com>
2016-08-29 10:23:24 +0530

    clean code commit

--- js/verge.js
+++ js/verge.js
@@ -1,9 +1,22 @@
-
 function opennav() {
     document.getElementById("nav-menu").style.transform= "translateX(0%)";
-
+    document.getElementById("nav-hidden-menu").style.transform= "translateX(0%)";
 }
 
 function closeNav() {
     document.getElementById("nav-menu").style.transform = "translateX(-100%)";
+    document.getElementById("nav-hidden-menu").style.transform = "translateX(-100%)";
+
+}
+
+function openarticles()
+{
+	document.getElementById("right-articles").style.transform= "translateX(-100%)";
+	document.getElementById("right-hidden-articles").style.transform= "translateX(-100%)";
+}
+
+function closearticles()
+{
+	document.getElementById("right-articles").style.transform= "translateX(0%)";
+	document.getElementById("right-hidden-articles").style.transform= "translateX(0%)";
 }
